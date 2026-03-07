daddf44972e5d82a1ba67b978dda79fbea5bb07c
massad alboqmi <massadalboqmi@MacBook-Air-massad.local>
2026-03-07 06:36:25 +0300

    Fix build error: remove .tsx extension from App import in main.tsx

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 import { Toaster } from '@/components/ui/sonner';
 createRoot(document.getElementById('root')).render(_jsxs(StrictMode, { children: [_jsx(App, {}), _jsx(Toaster, { position: "bottom-right", richColors: true })] }));
